2e67b2e40b365a9e8fb8b6907bfc2a9a0f31a5e1
Victor <victor@bmlabs.eu>
2024-08-30 19:02:50 +0200

    Start Search, add flatpickr

---
+++ .svelte-kit/generated/client/nodes/1.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.5.20_@sveltejs+vite-plugin-svelte@3.1.1_svelte@4.2.18_vite@5.3.5_@types+node@_mpfgph6km3gmipx764mbskxx3m/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
